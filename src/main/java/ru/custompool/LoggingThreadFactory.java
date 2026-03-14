31c365f9a973c80ec1cd1b93b06b4aae85a67d47
alexey-y-a <lexus2157@mail.ru>
2026-03-14 18:16:29 +0300

    step_003 done

---
+++ src/main/java/ru/custompool/LoggingThreadFactory.java
@@ -0,0 +1,25 @@
+package ru.custompool;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class LoggingThreadFactory implements ThreadFactory {
+    private static final Logger log = LoggerFactory.getLogger(LoggingThreadFactory.class);
+    private  final AtomicInteger threadNumber = new AtomicInteger(1);
+    private final String prefix;
+
+    public LoggingThreadFactory(String prefix) {
+        this.prefix = prefix;
+    }
+
+    @Override
+    public Thread newThread(Runnable r) {
+        String name = prefix + "-worker-" + threadNumber.getAndIncrement();
+        Thread t = new Thread(r, name);
+        log.info("[ThreadFactory] Creating new thread: {}", name);
+        return t;
+    }
+}
